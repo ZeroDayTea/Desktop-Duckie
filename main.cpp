463e768ee705ce302245735aa5d8322072a8d85b
ZeroDayTea <patrick.dobranowski@gmail.com>
2023-12-23 23:40:39 -0600

    basic movement and pausing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <unistd.h>
+#include <cstdlib>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #ifdef _WIN32
     #define GLFW_EXPOSE_NATIVE_WIN32
@@ -12,6 +15,14 @@
 #elif defined(__APPLE__)
     #include "macwindowsetup.h"
 #endif
+
+glm::vec3 genRandPosition() {
+    float x = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float y = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+    return {x, y, 0.0};
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -44,15 +55,14 @@ int main() {
         return -1;
     }
 
-    // make window transparent
-    glfwSetWindowOpacity(window, 1); // set to 1 for testing and 0 for borderless transparent window
-
-    // make window click-through and topmost (platform dependent)
+    // make window transparent, click-through and topmost (platform dependent)
 #ifdef _WIN32
     HWND hwnd = glfwGetWin32Window(window);
+    LONG style = GetWindowLong(hwnd, GWL_STYLE);
+    //SetWindowLong(hwnd, GWL_STYLE, style & ~WS_OVERLAPPEDWINDOW | WS_POPUP);
     LONG exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
     SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
-    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+    SetWindowPos(hwnd, HWND_TOPMOST, -5, -5, -5, -5, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
 #elif defined(__APPLE__)
     setMacWindowProperties(window);
 #endif
@@ -73,14 +83,14 @@ int main() {
     });
 
     // duck drawn to quad [3 positions, 3 colors, 2 texture coords]
-    float x = 0.5f;
-    float y = 0.5f;
+    float x = 0.025f;
+    float y = 0.025f;
     float vertices[] = {
-            x,  y, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-            x, -y, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-            -x, -y, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+            x,  y, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f,   // top right
+            x, -y, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f,   // bottom right
+            -x, -y, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f,   // bottom left
 
-            -x,  y, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+            -x,  y, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f    // top left
     };
     unsigned int indices[] = {
             0, 1, 3, // triangle 1
@@ -114,28 +124,22 @@ int main() {
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int duck_width, duck_height, nrChannels;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char *data = stbi_load("../assets/desktopduckie.png", &duck_width, &duck_height, &nrChannels, 0);
+    //stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("../assets/desktopduckie.png", &duck_width, &duck_height, &nrChannels, STBI_rgb_alpha);
 
     if(data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, duck_width, duck_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, duck_width, duck_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else {
         std::cout << "Failed to load texture" << std::endl;
         std::cerr << stbi_failure_reason() << std::endl;
-//        char cwd[1024];
-//        if (getcwd(cwd, sizeof(cwd)) != nullptr) {
-//            std::cout << "Current working dir: " << cwd << std::endl;
-//        } else {
-//            perror("getcwd() error");
-//        }
     }
     stbi_image_free(data);
 
@@ -149,10 +153,12 @@ int main() {
     out vec3 ourColor;
     out vec2 TexCoord;
 
+    uniform mat4 transform;
+
     void main() {
-        gl_Position = vec4(aPos, 1.0);
+        gl_Position = transform * vec4(aPos, 1.0);
         ourColor = aColor;
-        TexCoord = aTexCoord;
+        TexCoord = vec2(aTexCoord.x, aTexCoord.y);
     }
     )glsl";
 
@@ -186,12 +192,55 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    glm::vec3 currentPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 movingTowards = glm::vec3(0.3f, 0.3f, 0.0f);
+    float speed = 0.05f;
+
+    bool stopped = false;
+    float stopTime = 0.0f;
+    float stopMax = 15.0f;
+
+    glBindTexture(GL_TEXTURE_2D, texture);
 
     // main loop of the window
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+        // movement calculations
+        float deltaTime = glfwGetTime();
+        glfwSetTime(0.0);
+        float step = speed * deltaTime;
+
+        if(!stopped && glm::distance(currentPosition, movingTowards) > step) {
+            glm::vec3 direction = glm::normalize(movingTowards - currentPosition);
+            currentPosition += direction * step;
+        }
+        else if (!stopped) {
+            currentPosition = movingTowards;
+            stopped = true;
+            stopTime = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/stopMax));
+            movingTowards = genRandPosition();
+        }
+        else {
+            if (stopTime >= 0.0f) {
+                stopTime -= deltaTime;
+            }
+            else {
+                stopped = false;
+                stopTime = 0.0f;
+            }
+        }
+
+        // create textures
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, currentPosition);
+        //trans = glm::rotate(trans, currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+
         glUseProgram(shaderProgram);
+        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        // render duck quad container
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -199,6 +248,10 @@ int main() {
         glfwPollEvents();
     }
 
+    // deallocating and terminating
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
